dbed97331edb4ed09b8f06bcdafb46373a37522f
Tyler Luan <tylerluan@163.com>
2023-09-25 16:44:04 +0800

    salient detection onnx convert

--- modelscope/models/cv/salient_detection/salient_model.py
+++ modelscope/models/cv/salient_detection/salient_model.py
@@ -53,6 +53,19 @@ class SalientDetection(TorchModel):
                 torch.device([next(self.model.parameters()).device][0]))
 
         with torch.no_grad():
+            
+            def torch2onnx(model_, input_, output_name="./test.onnx"):
+                input_names = ["input_1"]
+                output_names = ["output_1"]
+                opset_version = 13
+                dynamic_axes = None
+                # dynamic_axes = {'input_1': [0, 2, 3], 'output_1': [0, 1]}
+                torch.onnx.export(model_, input_, output_name, verbose=True, opset_version=opset_version,
+                                  input_names=input_names, output_names=output_names,
+                                  dynamic_axes=dynamic_axes, do_constant_folding=True)
+                raise 'convert done !'
+                            
+            torch2onnx(self.model, data)
             results = self.model(data)
 
         if next(self.model.parameters()).is_cuda:
